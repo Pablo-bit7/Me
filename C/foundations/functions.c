b69949c47cdbf295450c338544d62e89c839b61c
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-07-31 04:58:40 +0200

    VS code

--- C/foundations/functions.c
+++ C/foundations/functions.c
@@ -10,4 +10,16 @@ void sayHi() /* 'void' tells the comp that this function doesn't returning anyth
 {
     printf("Hello user!");
 }
+/* to execute the code in the 'sayHi' function you'd need to CALL THE FUNCTION */
+
+
+/**
+* The 'int main()' is known as the main function/method but you can define functions as well
+* You have to tell the comp the return type of a function (e.g 'int' in 'int main()')
+*/
+int main()
+{
+    sayHi();
+    
+    return 0;
 }
