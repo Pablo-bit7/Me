1ed493cc324251508686f11d79f04aca9d84459f
Paballo Mogane <moganepaballo@gmail.com>
2025-07-21 21:35:21 +0200

    Completed compound_interest.py bootcamp question

---
+++ Python/compund_interest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Function calculates compund interest
+"""
+
+def compound_interest(starting_balance, interest_rate, contribution, months):
+    final_balance = starting_balance * (interest_rate) + contribution
+
+    for i in range(1, months):
+        final_balance = final_balance * (interest_rate) + contribution
+    
+    return final_balance
+
+if __name__ == '__main__':
+    starting_balance = float(input("Starting balance?: "))
+    interest_rate = float(input("Interest rate (decimal fraction)?: "))
+    contribution = float(input("Your monthly deposit?: "))
+    months = int(input("Number of months?: "))
+
+    print(compound_interest(starting_balance, interest_rate, contribution, months))
