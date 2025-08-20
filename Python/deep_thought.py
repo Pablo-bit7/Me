4fea0270f2966e403d56899903b036f4000b9403
Paballo Mogane <moganepaballo@gmail.com>
2025-08-20 23:32:20 +0200

    Deep Thought for CS50W

---
+++ Python/deep_thought.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+
+if answer.strip() == "42":
+    print("Yes")
+elif answer.lower() == "forty-two":
+    print("Yes")
+elif answer.lower() == "forty two":
+    print("Yes")
+else:
+    print("No")
