289208819d5c74d1c897306f64bae6fdd4e41681
Paballo Mogane <moganepaballo@gmail.com>
2025-08-25 23:51:04 +0200

    Initial commit for media type program for CS50P

---
+++ Python/extensions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Function takes a file name as input and returns the media type for that file based on its file extension
+"""
+
+def extension(file_name):
+    name_and_ext = file_name.lower().split(".")
+
+    if len(name_and_ext) == 1:
+        return "application/octet-stream"
+    elif name_and_ext[1] == "gif":
+        return "image/gif"
+    elif name_and_ext[1] == "jpg":
+        return "image/jpg"
+    elif name_and_ext[1] == "jpeg":
+        return "image/jpeg"
+    elif name_and_ext[1] == "png":
+        return "image/png"
+    elif name_and_ext[1] == "pdf":
+        return "application/pdf"
+    elif name_and_ext[1] == "txt":
+        return "text/plain"
+    elif name_and_ext[1] == "zip":
+        return "application/zip"
+
+
+if __name__ == '__main__':
+    for i in range(8):
+        file_name = input("File mane: ")
+        print(extension(file_name))
