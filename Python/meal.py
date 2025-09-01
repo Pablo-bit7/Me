d9e1215c94fffb776e29c037fb5f8474c1ff0ac7
Paballo Mogane <moganepaballo@gmail.com>
2025-09-01 21:12:26 +0200

    Refactored meal.pywith added / function

--- Python/meal.py
+++ Python/meal.py
@@ -6,27 +6,34 @@ Outputs whether it's breakfast, lunch, or dinner time
 def main():
     while True:
         time = input("Wha time is it? ")
-        print(convert(time))
+        
+        hrs_min = time.strip().split(":")
+        if hrs_min[0][0] == "0":
+            hrs_min[0] = hrs_min[0][1]
+
+        if hrs_min[0] == "7" and int(hrs_min[1]) in range(60):
+            print("breakfast time")
+        elif hrs_min[0] == "8" and hrs_min[1] == "00":
+            print("breakfast time")
+        elif hrs_min[0] == "12" and int(hrs_min[1]) in range(60):
+            print("lunch time")
+        elif hrs_min[0] == "13" and hrs_min[1] == "00":
+            print("lunch time")
+        elif hrs_min[0] == "18" and int(hrs_min[1]) in range(60):
+            print("dinner time")
+        elif hrs_min[0] == "19" and hrs_min[1] == "00":
+            print("dinner time")
 
 def convert(time):
     hrs_min = time.strip().split(":")
+
     if hrs_min[0][0] == "0":
         hrs_min[0] = hrs_min[0][1]
-    #if hrs_min[1][0] == "0":
-    #    hrs_min[1] = hrs_min[1][1]
+    if hrs_min[1][0] == "0":
+        hrs_min[1] = hrs_min[1][1]
 
-    if hrs_min[0] == "7" and int(hrs_min[1]) in range(60):
-        return "breakfast time"
-    elif hrs_min[0] == "8" and hrs_min[1] == "00":
-        return "breakfast time"
-    elif hrs_min[0] == "12" and int(hrs_min[1]) in range(60):
-        return "lunch time"
-    elif hrs_min[0] == "13" and hrs_min[1] == "00":
-        return "lunch time"
-    elif hrs_min[0] == "18" and int(hrs_min[1]) in range(60):
-        return "dinner time"
-    elif hrs_min[0] == "19" and hrs_min[1] == "00":
-        return "dinner time"
+    answer = int(hrs_min[0]) + int(hrs_min[1]) / 60
+    return f"{answer} hours"
 
 
 if __name__ == "__main__":
