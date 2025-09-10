e2025bb389145ea241c496acf5824ca6ca391ffd
Paballo Mogane <moganepaballo@gmail.com>
2025-09-10 23:52:29 +0200

    Initial commit for `taqueria.py` for CS50P

---
+++ Python/taqueria.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Program prompts user for items on a menu and outputs the total cost
+"""
+
+
+def main():
+    menu = {
+        "Baja Taco": 4.25,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00
+    }
+    amount_due = 0
+
+    while True:
+        try:
+            order = input("Item: ").strip()
+            amount_due += menu[order.title()]
+
+        except ValueError:
+            continue
+        except KeyError:
+            continue
+        except EOFError:
+            print(f"\nTotal: {amount_due:.2f}$")
+            break
+
+
+if __name__ == "__main__":
+    main()
