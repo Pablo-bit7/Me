3556bc3e42c29240303e20de54bd3794c12f8228
Paballo Mogane <moganepaballo@gmail.com>
2025-09-20 23:50:52 +0200

    Initial commit for `emojize.py`

---
+++ Python/emojize.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Prompts user for a str in English and outputs the “emojized” version
+"""
+import emoji
+
+
+def main():
+    try:
+        usr_npt = input("Input: ")
+
+        if "_" in usr_npt:
+            ...
+        
+        else:
+            ...
+    
+    except ValueError:
+        return
+    except EOFError:
+        return
+    
+    else:
+        ...
+
+
+if __name__ == "__main__":
+    main()
