05ab62c9dba6bc3f879522d8b6e9f086c8e38d9d
Paballo Mogane <moganepaballo@gmail.com>
2025-10-01 23:56:45 +0200

    Initial commit of guessing game `game.py`

---
+++ Python/game.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Simple guessing game
+"""
+import random
+
+
+def main():
+    while True:
+        try:
+            usr_npt = int(input("Level: ").strip())
+
+            if usr_npt > 0:
+                level = random.randrange(1, usr_npt + 1)
+
+                while True:
+                    try:
+                        usr_guess = int(input("Guess: ").strip())
+
+                        if usr_guess > 0:
+                            if usr_guess == level:
+                                break
+                            elif usr_guess > level:
+                                print("Too large!")
+                                continue
+                            else:
+                                print("Too small!")
+                                continue
+                        else:
+                            continue
+
+                    except ValueError:
+                        continue
+
+                break
+            else:
+                continue      
+
+        except ValueError:
+            continue
+
+
+if __name__ == '__main__':
+    main()
