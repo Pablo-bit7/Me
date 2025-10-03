8e528cae5d4de967a493ab5a71ace1d3603e4a84
Paballo Mogane <moganepaballo@gmail.com>
2025-10-03 23:50:49 +0200

    Fixed `generate_integer()` bug

--- Python/professor.py
+++ Python/professor.py
@@ -7,7 +7,35 @@ import random
 
 
 def main():
-    ...
+    prb_count = 0
+    ans_count = 0
+    n = get_level()
+    score = 0
+
+    while prb_count != 10:
+        x = generate_integer(n)
+        y = generate_integer(n)
+
+        while ans_count != 3:
+            try:
+                ans = int(input(f"{x} + {y} = ").strip())
+
+                if ans == (x + y):
+                    score += 1
+                    break
+                else:
+                    ans_count += 1
+                    continue
+    
+            except ValueError:
+                print("EEE")
+                ans_count += 1
+                continue
+
+        if ans_count == 3:
+            print(f"{x} + {y} = {x + y}")
+
+        prb_count += 1
 
 
 def get_level():
@@ -25,7 +53,10 @@ def get_level():
 
 
 def generate_integer(level):
-    return random.randint(level)
+    rng_start = 10 ** (level - 1)
+    rng_end = (10 ** level) -1
+
+    return random.randint(rng_start, rng_end)
 
 
 if __name__ == "__main__":
