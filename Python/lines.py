d18acef2f4f3aadb1fffac13c78d296136ba38a5
Paballo Mogane <moganepaballo@gmail.com>
2025-10-23 23:44:20 +0200

    Initial commit of `lines.py`

---
+++ Python/lines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Program takes 1 command-line argument (name/path of a file) and opuputs
+the number of lines of code in the file (excluding comments and blank lines)
+"""
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
