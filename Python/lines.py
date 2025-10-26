973f7667d695897b266e240c074b8454aa290d73
Paballo Mogane <moganepaballo@gmail.com>
2025-10-26 23:41:28 +0200

    Finished implementing line counting logic

--- Python/lines.py
+++ Python/lines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Program takes 1 command-line argument (name/path of a file) and opuputs
+Program takes 1 command-line argument (name/path of file) and opuputs
 the number of lines of code in the file (excluding comments and blank lines)
 """
 import sys
@@ -11,7 +11,7 @@ def main():
     if len(sys.argv) != 2:
         sys.exit("Too few command-line arguments")
 
-    if len(sys.argv) > 2:
+    elif len(sys.argv) > 2:
         sys.exit("Too many command-line arguments")
 
     file_path = sys.argv[1]
@@ -19,8 +19,23 @@ def main():
     if not os.path.exists(file_path):
         sys.exit("File does not exist")
 
-    if not os.path.endswith(".py"):
-        
+    if not file_path.endswith(".py"):
+        sys.exit("Not a python file")
+
+    try:
+        with open(file_path, "r") as file:
+            lines = file.readlines()
+    except (OSError, IOError):
+        sys.exit("Could not read file")
+
+    num_lines = 0
+
+    for line in lines:
+        line = line.strip()
+        if line and not line.startswith("#"):
+            num_lines += 1
+
+    print(num_lines)
 
 
 if __name__ == "__main__":
