a9a00e7b03e563813a165c2a9f2e6d2999e2605f
Paballo Mogane <moganepaballo@gmail.com>
2025-10-30 23:25:33 +0200

    Completed csv read logic

--- Python/pizza.py
+++ Python/pizza.py
@@ -4,13 +4,15 @@ Program takes 1 command-line argument (name/path of CSV file) and outputs
 a table formatted as ASCII
 """
 import sys
+import csv
+from tabulate import tabulate
 
 
 def main():
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         sys.exit("Too few command-line arguments")
 
-    if len(sys.argv) > 2:
+    elif len(sys.argv) > 2:
         sys.exit("Too many command-line arguments")
 
     file_path = sys.argv[1]
@@ -19,10 +21,13 @@ def main():
         sys.exit("Not a CSV file")
 
     try:
-        with open(file_path, newline="") as file:
-            ...
+        with open(file_path, newline="", encoding="utf-8") as csvfile:
+            reader = csv.DictReader(csvfile)
+            rows = [row for row in reader]
     except FileNotFoundError:
         sys.exit("File not found")
+
+    print(tabulate(rows, headers="keys", tablefmt="grid"))
 
 
 if __name__ == "__main__":
