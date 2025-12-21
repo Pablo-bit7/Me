3f8aa0d70897562840cbca605e50e8c8ac145a92
Paballo Mogane <moganepaballo@gmail.com>
2025-12-21 23:45:43 +0200

    Initial commit of boilerplate for the `shirtificate.py` program

---
+++ Python/shirtificate.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Program prompts for a name and outputs a PDF of a shirt with text
+overlaid on it, as well as a heading. 
+"""
+
+
+...
+
+
+if __name__ == "__main__":
+    ...
